9cc47315220d02de0613b6e19b239d4fe2a111dd
jooyeal <34053954+jooyeal@users.noreply.github.com>
2021-08-11 10:06:57 +0900

    Updates

--- src/text/SimpleTextField.js
+++
@@ -1,5 +0,0 @@
-export const text = `<TextField />
-<TextField label="label" />
-<TextField label="label" width="5rem" />
-<TextField label="label" disabled />
-<TextField label="label" color="#EF5350" />`;
